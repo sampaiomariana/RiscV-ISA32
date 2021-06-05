a37667851476dc70a85617a9b7243e7ca64d4f57
mariana <marisampa26@gmail.com>
2021-06-05 16:23:09 -0300

    add adder.vhd e adder4.vhd

---
+++ RiscV/adder.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+
+entity adder is
+  port (
+    A : in std_logic_vector(31 downto 0);
+    B : in std_logic_vector(31 downto 0);
+    Z : out std_logic_vector(31 downto 0));
+end entity adder;
+
+architecture arch_adder OF adder IS
+
+begin
+
+  Z <= std_logic_vector(UNSIGNED(A) + UNSIGNED(B));
+
+end arch_adder;
---
+++ RiscV/adder4.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+
+entity adder4 is
+  port (
+    A : in std_logic_vector(31 downto 0);
+    Z : out std_logic_vector(31 downto 0));
+end entity adder4;
+
+architecture arch_adder4 OF adder4 is
+
+signal A_signal : INTEGER;
+
+begin
+
+  A_signal <= TO_INTEGER(UNSIGNED(A)) + 4;
+  Z <= std_logic_vector(TO_UNSIGNED(A_signal, Z'length));
+
+end arch_adder4;
